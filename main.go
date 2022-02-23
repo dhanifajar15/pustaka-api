fc38a0ea1e540ee23f81e9c6b462b2ada768e203
dhanifajar15 <dhanifajar15@gmail.com>
2022-02-23 21:19:42 +0700

    connect to database

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	router := gin.Default()
+	dsn := "dhanifajar15:dhani@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	if err != nil {
+		log.Fatal("Database connection error")
+	}
+
+	fmt.Println("Database succeed")
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
