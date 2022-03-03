9e73e7a702e0bd6deb4dc3b163c20bd8885325a8
dhanifajar15 <dhanifajar15@gmail.com>
2022-03-03 12:25:29 +0700

    update

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"pustaka-api/handler"
-
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"pustaka-api/book"
+	"pustaka-api/handler"
 )
 
 func main() {
 	router := gin.Default()
-	dsn := "dhanifajar15:dhani@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "dhanifajar15:dhani@tcp(192.168.1.7:6033)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Database connection error")
 	}
+	db.AutoMigrate(&book.Book{})
+	//CRUD
 
-	fmt.Println("Database succeed")
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
