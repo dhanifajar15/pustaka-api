97dd793827171e2e18613b607df396611533ad86
dhanifajar15 <dhanifajar15@gmail.com>
2022-03-03 14:29:15 +0700

    take data

--- main.go
+++ main.go
@@ -21,20 +21,50 @@ func main() {
 	db.AutoMigrate(&book.Book{})
 	//CRUD
 
-	book := book.Book{}
-	book.Title = "Atomic Habits"
-	book.Price = 12000
-	book.Discount = 10
-	book.Rating = 12
-	book.Description = "Buku tentang membangun kebiasaan baik dan menghilangkan kebiasaan buruk"
+	//book := book.Book{}
+	//book.Title = "Atomic Habits"
+	//book.Price = 12000
+	//book.Discount = 10
+	//book.Rating = 12
+	//book.Description = "Buku tentang membangun kebiasaan baik dan menghilangkan kebiasaan buruk"
 
-	err = db.Create(&book).Error
+	//err = db.Create(&book).Error
 
+	//if err != nil {
+	//	fmt.Println("===================")
+	//	fmt.Println("Error Creating Book")
+	//	fmt.Println("===================")
+	//}
+
+	//var book book.Book
+	//err = db.First(&book).Error
+
+	//select first record
+	//err = db.Debug().First(&book).Error
+
+	//select last record
+
+	//err = db.Debug().Last(&book).Error
+
+	var books []book.Book
+
+	//find
+	//err = db.Debug().Find(&books).Error
+	err = db.Debug().Where("rating = ?", 5).Find(&books).Error
 	if err != nil {
 		fmt.Println("===================")
-		fmt.Println("Error Creating Book")
+		fmt.Println("Error finding book record")
 		fmt.Println("===================")
 	}
+
+	for _, b := range books {
+		fmt.Println("Title : ", b.Title)
+		fmt.Println("book object %v", b)
+	}
+
+	//fmt.Println("Title: ", book.Title)
+	//fmt.Println("book object %v", book)
+
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
